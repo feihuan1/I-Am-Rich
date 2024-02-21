f63f474e09128adde1ff8989b3cc678d2f4fdf6f
feihuan1 <ericrealtor1997@gmail.com>
2024-02-21 06:34:40 -0800

    Initial Commit

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Feihuan Peng on 2/21/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
